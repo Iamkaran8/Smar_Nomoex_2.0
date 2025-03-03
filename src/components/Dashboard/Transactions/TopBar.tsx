fb5932ed35368681969e2ad368273c0d44dcf173
Iamkaran8 <karanwebdev8@gmail.com>
2025-03-03 17:56:35 +0530

    Feat : State Management

--- src/components/Dashboard/Transactions/TopBar.tsx
+++ src/components/Dashboard/Transactions/TopBar.tsx
@@ -2,18 +2,20 @@ import { CoinConversion } from "./CoinConversion";
 import { CoinExplorer } from "./CoinExplorer";
 import { SearchBar } from "./SearchBar";
 
-
 export const TopBar = () => {
-  
   return (
     <>
       <div className="cont flex ">
         <div className="w-[50%] flex gap-3">
-            <CoinExplorer/>
-            <CoinConversion/>    
+          <div className="w-[50%]">
+            <CoinExplorer />
+          </div>
+          <div className="w-[50%] relative">
+            <CoinConversion />
+          </div>
         </div>
         <div className="w-[50%] flex justify-end">
-            <SearchBar/>
+          <SearchBar />
         </div>
       </div>
     </>
