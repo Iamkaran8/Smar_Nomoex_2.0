b8563965f6600486ad75c2755d171d83ddf2a3c6
Iamkaran8 <karanwebdev8@gmail.com>
2025-03-03 12:32:21 +0530

    feat : Data management

--- src/components/Dashboard/Transactions/CoinExplorer.tsx
+++ src/components/Dashboard/Transactions/CoinExplorer.tsx
@@ -3,7 +3,7 @@ import img from "../../../assets/Dummy_images/Bitcoin.svg";
 export const CoinExplorer = () => {
   return (
     <>
-      <div className="flex realative justify-evenly items-center p-3 w-[50%] border-[1px] border-Soft_Gray_20 rounded-[6px]  hover:bg-Soft_Gray_20 ease-in duration-300">
+      <div className="flex relative justify-evenly items-center p-3 w-[50%] border-[1px] border-Soft_Gray_20 rounded-[6px]  hover:bg-Soft_Gray_20 ease-in duration-300">
         <img src={img} alt="btc" />
         <h4 className="font-medium">Bitcoin Explorer</h4>
         <svg
@@ -15,6 +15,7 @@ export const CoinExplorer = () => {
           <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
         </svg>
       </div>
+
     </>
   );
 };
